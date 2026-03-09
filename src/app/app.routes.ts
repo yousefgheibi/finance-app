b41cbc1fa7bea93bd75f6c5f0adf56e9bfdd1c3c
Yousef Gheibi <gheibiyousef2000@gmail.com>
2026-03-09 17:18:12 +0330

    added AuthGuard function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
-    { path: 'dashboard', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+    { path: 'dashboard', loadChildren: () => import('./core/core.module').then(m => m.CoreModule), canActivate: [AuthGuard] },
     { path: '**', redirectTo: 'login' }
 ];
