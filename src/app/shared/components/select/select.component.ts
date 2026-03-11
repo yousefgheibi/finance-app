606bd7417b459da8aa6efe128e292353f96a5c8d
Yousef Gheibi <gheibiyousef2000@gmail.com>
2026-03-11 21:36:40 +0330

    Added select component

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,21 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, input, Optional, Self, signal } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { SelectOption } from '../../models/option.model';
 
 @Component({
-  selector: 'app-text-input',
+  selector: 'app-select',
   imports: [CommonModule],
-  templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.scss']
+  templateUrl: './select.component.html',
+  styleUrl: './select.component.scss'
 })
-export class TextInputComponent implements ControlValueAccessor {
+export class SelectComponent implements ControlValueAccessor {
   id = input<string>('');
   label = input<string>('');
   placeholder = input<string>('');
-  type = input<string>('text');
-  readonly = input<boolean>(false);
   required = input<boolean>(false);
-
+  options = input<SelectOption[]>([]);
   value = signal<string>('');
   disabled = signal<boolean>(false);
 
@@ -44,9 +43,9 @@ export class TextInputComponent implements ControlValueAccessor {
     this.disabled.set(isDisabled);
   }
 
-  onInput(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    this.value.set(input.value);
+  onChangeSelect(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    this.value.set(select.value);
     this.onChange(this.value());
   }
 
@@ -77,12 +76,6 @@ export class TextInputComponent implements ControlValueAccessor {
     switch (firstErrorKey) {
       case 'required':
         return 'این فیلد الزامی است.';
-      case 'email':
-        return 'لطفا یک آدرس ایمیل معتبر وارد کنید.';
-      case 'minlength':
-        return `حداقل طول باید ${errors['minlength']?.requiredLength} کاراکتر باشد.`;
-      case 'maxlength':
-        return `حداکثر طول باید ${errors['maxlength']?.requiredLength} کاراکتر باشد.`;
       default:
         return 'این فیلد نامعتبر است.';
     }
