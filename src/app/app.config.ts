2ea6632ca7c58685dc2cab8bbc06ea496902f344
Yousef Gheibi <gheibiyousef2000@gmail.com>
2026-03-30 15:21:28 +0330

    Added view-transition to route switching

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
@@ -7,7 +7,7 @@ import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
+    provideRouter(routes, withViewTransitions()), 
     provideHttpClient(),
     provideCharts(withDefaultRegisterables())
   ]
