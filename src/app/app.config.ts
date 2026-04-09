bcc907570e800a51e629b4182d61aa228743814b
Yousef Gheibi <gheibiyousef2000@gmail.com>
2026-04-09 12:09:46 +0330

    Set scrollPositionRestoration top

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
@@ -7,7 +7,7 @@ import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes, withViewTransitions()), 
+    provideRouter(routes, withViewTransitions(),withInMemoryScrolling({ scrollPositionRestoration:'top'})), 
     provideHttpClient(),
     provideCharts(withDefaultRegisterables())
   ]
