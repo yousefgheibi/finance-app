a6551b7d043e28bab232f7ee0bae1bd1d99604c1
Yousef Gheibi <gheibiyousef2000@gmail.com>
2026-04-09 12:20:04 +0330

    Added PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, withViewTransitions(),withInMemoryScrolling({ scrollPositionRestoration:'top'})), 
     provideHttpClient(),
-    provideCharts(withDefaultRegisterables())
+    provideCharts(withDefaultRegisterables()), 
+    provideServiceWorker('ngsw-worker.js', 
+      {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+      })
   ]
 };
